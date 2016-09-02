a395a53980b2045ad58e5a138e7a9ea3c022595a
Nikita <nikita.lipatov@tele2.com>
2016-09-02 12:50:42 +0300

    jaxb-tests were added

--- src/main/java/org/examples/xsd2model/services/JaxbServices.java
+++ src/main/java/org/examples/xsd2model/services/JaxbServices.java
@@ -1,13 +1,10 @@
 package org.examples.xsd2model.services;
 
-import org.examples.xsd2model.model.request.RequestType;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.xml.bind.*;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
+import javax.xml.stream.*;
 import java.io.Reader;
 import java.io.StringReader;
 import java.io.StringWriter;
@@ -18,36 +15,38 @@ import java.io.Writer;
  * JAXBContext is thread safe and should only be created once and reused to avoid the cost of initializing the metadata multiple times.
  * Marshaller and Unmarshaller are not thread safe, but are lightweight to create and could be created per operation.
  */
-public class JaxbServices {
+public class JaxbServices<T> {
 
     private final static Logger log = LoggerFactory.getLogger(JaxbServices.class);
 
+    private Class clazz;
     private JAXBContext context;
 
-    public JaxbServices() throws JAXBException {
-        context = JAXBContext.newInstance(RequestType.class);
+    public <T> JaxbServices(Class<T> clazz) throws JAXBException {
+        this.clazz = clazz;
+        context = JAXBContext.newInstance(this.clazz);
     }
 
-    public String object2xml(RequestType requestType) throws JAXBException, XMLStreamException {
+    public <T> String object2xml(T type) throws JAXBException, XMLStreamException {
         Writer writer = new StringWriter();
 
         Marshaller marshaller = context.createMarshaller();
         marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, Boolean.TRUE);
-        marshaller.marshal(requestType, writer);
+        marshaller.marshal(type, writer);
 
         return writer.toString();
     }
 
     // Without @XmlRootElement annotation we can use this code instead of above
-    public String object2xml2(RequestType requestType) throws JAXBException, XMLStreamException {
+    public <T> String object2xml2(T type) throws JAXBException, XMLStreamException {
         Writer writer = new StringWriter();
 
-        JAXB.marshal(requestType, writer);
+        JAXB.marshal(type, writer);
 
         return writer.toString();
     }
 
-    public RequestType xml2object(String xml) throws JAXBException, XMLStreamException  {
+    public <T> T xml2object(String xml) throws JAXBException, XMLStreamException  {
         // xml to XmlStreamReader
         Reader reader = new StringReader(xml);
         XMLInputFactory factory = XMLInputFactory.newInstance();
@@ -55,9 +54,9 @@ public class JaxbServices {
 
         Unmarshaller unmarshaller = context.createUnmarshaller();
 
-        JAXBElement jaxbElement = unmarshaller.unmarshal(xmlReader, RequestType.class);
+        JAXBElement jaxbElement = unmarshaller.unmarshal(xmlReader, this.clazz);
 
-        return (RequestType)jaxbElement.getValue();
+        return (T)jaxbElement.getValue();
     }
 
 }
--- src/test/java/org/examples/xsd2model/services/JaxbServicesTest.java
+++ src/test/java/org/examples/xsd2model/services/JaxbServicesTest.java
@@ -3,6 +3,8 @@ package org.examples.xsd2model.services;
 import org.examples.xsd2model.model.UserType;
 import org.examples.xsd2model.model.request.Message;
 import org.examples.xsd2model.model.request.RequestType;
+import org.examples.xsd2model.model.response.Body;
+import org.examples.xsd2model.model.response.ResponseType;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -16,12 +18,16 @@ import java.nio.file.Paths;
  */
 public class JaxbServicesTest {
 
-    private JaxbServices jaxbServices;
+    private JaxbServices coreServices;
+    private JaxbServices requestServices;
+    private JaxbServices responseServices;
     private UserType userType;
 
     @Before
     public void setUp() throws JAXBException {
-        jaxbServices = new JaxbServices();
+        coreServices = new JaxbServices(UserType.class);
+        requestServices = new JaxbServices(RequestType.class);
+        responseServices = new JaxbServices(ResponseType.class);
 
         UserType userType = new UserType();
         userType.setLogin("nikita");
@@ -30,7 +36,30 @@ public class JaxbServicesTest {
     }
 
     @Test
-    public void testObject2Xml2Object2Xml() throws Exception {
+    public void testCoreObject2Xml2Object2Xml() throws Exception {
+
+        // toXml
+        String xml1 = coreServices.object2xml(this.userType);
+        // toObject
+        UserType userType2 = (UserType) coreServices.xml2object(xml1);
+        // toXml
+        String xml2 = coreServices.object2xml(userType2);
+        // toXml using different method
+        UserType userType3 = (UserType) coreServices.xml2object(xml2);
+        String xml3 = coreServices.object2xml2(userType3);
+
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                "<ns2:userType xmlns:ns2=\"http://xsd2model.org/examples/xsd2model/model/core\">\n" +
+                "    <login>nikita</login>\n" +
+                "    <password>password22</password>\n" +
+                "</ns2:userType>\n";
+        Assert.assertEquals(xml, xml1);
+        Assert.assertEquals(xml1, xml2);
+        Assert.assertEquals(xml2, xml3);
+    }
+
+    @Test
+    public void testRequestObject2Xml2Object2Xml() throws Exception {
         // object
         RequestType requestType1 = new RequestType();
         requestType1.setUser(userType);
@@ -40,26 +69,65 @@ public class JaxbServicesTest {
         requestType1.setMessage(message);
 
         // toXml
-        String xml1 = jaxbServices.object2xml(requestType1);
+        String xml1 = requestServices.object2xml(requestType1);
         // toObject
-        RequestType requestType2 = jaxbServices.xml2object(xml1);
+        RequestType requestType2 = (RequestType) requestServices.xml2object(xml1);
         // toXml
-        String xml2 = jaxbServices.object2xml(requestType2);
+        String xml2 = requestServices.object2xml(requestType2);
         // toXml using different method
-        RequestType requestType3 = jaxbServices.xml2object(xml1);
-        String xml3 = jaxbServices.object2xml2(requestType3);
+        RequestType requestType3 = (RequestType) requestServices.xml2object(xml2);
+        String xml3 = requestServices.object2xml2(requestType3);
 
         String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
-                "<ns2:requestType xmlns=\"http://xsd2model.org/examples/xsd2model/model/core\" xmlns:ns2=\"http://xsd2model.org/examples/xsd2model/model/request\">\n" +
-                "    <ns2:user>\n" +
+                "<ns3:requestType xmlns:ns2=\"http://xsd2model.org/examples/xsd2model/model/core\" xmlns:ns3=\"http://xsd2model.org/examples/xsd2model/model/request\">\n" +
+                "    <user>\n" +
                 "        <login>nikita</login>\n" +
                 "        <password>password22</password>\n" +
-                "    </ns2:user>\n" +
-                "    <ns2:message>\n" +
-                "        <ns2:log>log-1234567</ns2:log>\n" +
-                "        <ns2:command>mkdir test</ns2:command>\n" +
-                "    </ns2:message>\n" +
-                "</ns2:requestType>\n";
+                "    </user>\n" +
+                "    <message>\n" +
+                "        <log>log-1234567</log>\n" +
+                "        <command>mkdir test</command>\n" +
+                "    </message>\n" +
+                "</ns3:requestType>\n";
+        Assert.assertEquals(xml, xml1);
+        Assert.assertEquals(xml1, xml2);
+        Assert.assertEquals(xml2, xml3);
+    }
+
+    @Test
+    public void testResponseObject2Xml2Object2Xml() throws Exception {
+        // object
+        ResponseType responseType1 = new ResponseType();
+        responseType1.setUser(userType);
+        Message message = new Message();
+        message.setLog("log-1234567");
+        message.setCommand("mkdir test");
+        Body body = new Body();
+        body.setCommand("rm -r");
+        body.setLog("done");
+        responseType1.setBody(body);
+
+        // toXml
+        String xml1 = responseServices.object2xml(responseType1);
+        // toObject
+        ResponseType responseType2 = (ResponseType)responseServices.xml2object(xml1);
+        // toXml
+        String xml2 = responseServices.object2xml(responseType2);
+        // toXml using different method
+        ResponseType responseType3 = (ResponseType) responseServices.xml2object(xml2);
+        String xml3 = responseServices.object2xml2(responseType3);
+
+        String xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>\n" +
+                "<ns3:responseType xmlns:ns2=\"http://xsd2model.org/examples/xsd2model/model/core\" xmlns:ns3=\"http://xsd2model.org/examples/xsd2model/model/response\">\n" +
+                "    <user>\n" +
+                "        <login>nikita</login>\n" +
+                "        <password>password22</password>\n" +
+                "    </user>\n" +
+                "    <Body>\n" +
+                "        <log>done</log>\n" +
+                "        <command>rm -r</command>\n" +
+                "    </Body>\n" +
+                "</ns3:responseType>\n";
         Assert.assertEquals(xml, xml1);
         Assert.assertEquals(xml1, xml2);
         Assert.assertEquals(xml2, xml3);
@@ -70,9 +138,9 @@ public class JaxbServicesTest {
         final String xmlResponse = new String(Files.readAllBytes(Paths.get("src/test/resources/performance.xml")));
         long startTime = System.currentTimeMillis();
         for (int i = 0; i < 150; i++) {
-            RequestType requestType = jaxbServices.xml2object(xmlResponse);
-            Assert.assertNotNull(requestType.getUser());
-            Assert.assertNotNull(requestType.getUser().getLogin());
+            ResponseType responseType = (ResponseType)responseServices.xml2object(xmlResponse);
+            Assert.assertNotNull(responseType.getUser());
+            Assert.assertNotNull(responseType.getUser().getLogin());
         }
         long endTime = System.currentTimeMillis();
 
